41af396adb69e1577473af0eea89c231a645b5c7
donbay28 <donbaysaputra28@gmail.com>
2020-12-12 19:20:53 +0700

    create kategori views

---
+++ resources/views/Kategori/create.blade.php
@@ -0,0 +1,62 @@
+@include('Template/Header')
+@include('Template/Body')
+<div class="page-wrapper">
+  <div class="container-fluid">
+      <div class="row page-titles">
+          <div class="col-md-5 align-self-center">
+              <h4 class="text-themecolor">Kategori Dompet</h4>
+          </div>
+          <div class="col-md-7 align-self-center text-right">
+              <div class="d-flex justify-content-end align-items-center">
+                  <ol class="breadcrumb">
+                      <li class="breadcrumb-item"><a href="{{url('/home')}}">Home</a></li>
+                      <li class="breadcrumb-item active">Kategori Buat Baru</li>
+                  </ol>
+              </div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-12">
+              <div class="card">
+                  <div class="card-body">
+                      <div class="table">
+                          <form action="{{ url('/kategoris/store') }}" method="post" enctype="multipart/form-data">
+                          @csrf
+                          <div class="row">
+                              <div class="col-md-6">
+                                  <div class="form-group">
+                                      <label class="control-label">Nama* : </label>
+                                      <input type="text" placeholder="Nama" minlength="5" required class="form-control" name="nama">
+                                  </div>
+                              </div>
+                              <div class="col-md-12">
+                                  <div class="form-group">
+                                      <label class="control-label">Deskripsi : </label>
+                                      <textarea name="deskripsi" class="form-control"></textarea>
+                                  </div>
+                              </div>
+                              <div class="col-md-6">
+                                  <div class="form-group">
+                                      <label class="control-label">Status : </label>
+                                      <select name="status_id" class="form-control">
+                                        <option value="1">Aktif</option>
+                                        <option value="2">Tidak Aktif</option>
+                                      </select>
+                                  </div>
+                              </div>
+                              <div class="col-md-12">
+                                  <div class="full-width text-center p-t-12" >
+                                      <button onclick="window.history.back();" type="button" class="btn btn-info">Back</button>
+                                      <button class="btn btn-success" type="submit">Simpan</button>
+                                  </div>
+                              </div>
+                          </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+@include('Template/Footer')
---
+++ resources/views/Kategori/index.blade.php
@@ -0,0 +1,112 @@
+@include('Template/Header')
+@include('Template/Body')
+    <div class="page-wrapper">
+      <div class="container-fluid">
+          <div class="row page-titles">
+              <div class="col-md-5 align-self-center">
+                  <h4 class="text-themecolor">Master Kategori</h4>
+              </div>
+              <div class="col-md-7 align-self-center text-right">
+                  <div class="d-flex justify-content-end align-items-center">
+                      <ol class="breadcrumb">
+                          <li class="breadcrumb-item">Home</li>
+                          <li class="breadcrumb-item active">Data Kategori</li>
+                      </ol>
+                  </div>
+              </div>
+          </div>
+          <div class="row">
+
+              <div class="col-12">
+                  <div class="card">
+                      <div class="card-body">
+                      <div class="row">
+                          <div class="col-md-5">
+                            <div class="form-group">
+                              <a class="btn btn-info" href="{{url('/kategoris/create')}}" role="button"><i class="fa fa-plus-circle"></i>Buat Baru</a>
+                              <select class="btn btn-md btn-info" id="filter-status">
+                                <option value="0">Semua</option>
+                                <option value="1">Aktif</option>
+                                <option value="2">Tidak Aktif</option>
+                              </select>
+                            </div>
+                          </div>
+                      </div>
+                      <br>
+                      <div class="table-responsive">
+                          <table class="table table-bordered" id="dataTable">
+                          <thead>
+                              <tr>
+                                <th>No</th>
+                                <th>Name</th>
+                                <th>Deskripsi</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              @if($data_kategori ?? '' != null)
+                                  @foreach($data_kategori ?? '' ?? '' as $key => $row)
+                                      <tr>
+                                        <td>{{ ++$key}}</td>
+                                        <td>{{$row->nama}}</td>
+                                        <td>{{$row->deskripsi}}</td>
+                                        <td>
+                                          @if ($row->status_id == 1)
+                                              Aktif
+                                          @else
+                                              Tidak Aktif
+                                          @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{url('/kategoris/show/'.$row->id)}}" class="btn btn-sm btn-success" data-toggle="tooltip" data-placement="top" title="Detail"><i class="fa fa-eye"></i> Detail</a>
+                                            <a href="{{url('/kategoris/edit/'.$row->id)}}" class="btn btn-sm btn-warning" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pen"></i> Edit</a>
+                                            @if ($row->status_id == 1)
+                                              <a href="{{url('/kategoris/changeStatus/'.$row->id.'/'.$row->status_id)}}" class="btn btn-sm btn-danger" data-toggle="tooltip" data-placement="top" title="Change To Tidak Aktif"><i class="fa fa-times"></i> Tidak Aktif</a>
+                                            @else
+                                              <a href="{{url('/kategoris/changeStatus/'.$row->id.'/'.$row->status_id)}}" class="btn btn-sm btn-info" data-toggle="tooltip" data-placement="top" title="Change To Aktif"><i class="fa fa-check"></i> Aktif</a>
+                                            @endif
+                                        </td>
+                                      </tr>
+                                  @endforeach
+                              @else
+                              <tr><td>Data Not Found</td></tr>
+                              @endif
+                              </tbody>
+                          </table>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+
+      </div>
+      <!-- ============================================================== -->
+      <!-- End Container fluid  -->
+      <!-- ============================================================== -->
+    </div>
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable();
+        });
+        $(document).on("change", "#filter-status", function(e) {
+            status_id = $('#filter-status').val();
+            $.ajax({
+                type: 'POST',
+                url: '/dompets/filterStatus/'+status_id,
+                data: {
+                    "status_id": status_id,
+                    '_token': '{{ csrf_token() }}',
+                }
+            })
+            .done(function(response){
+                console.log(response)
+                $('#dataTable').DataTable().ajax.reload();
+            })
+            .fail(function(){
+                bootbox.alert('Something Went Wrong .... Please contact administrator');
+            })
+        });
+    </script>
+@include('Template/Footer')
