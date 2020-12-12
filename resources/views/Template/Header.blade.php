924e3989b49014fa782767debfbba79b2e1501a6
donbay28 <donbaysaputra28@gmail.com>
2020-12-12 19:31:42 +0700

    Update template header

--- resources/views/Template/Header.blade.php
+++ resources/views/Template/Header.blade.php
@@ -11,15 +11,17 @@
 
   <title>Atom Pockets</title>
   <script src="{{url('templates/assets/jquery/jquery.min.js')}}"></script>
+  <script src="{{url('templates/assets/plugins/datetimepicker-master/jquery.datetimepicker.js')}}"></script>
+  
   <link href="{{url('templates/assets/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
   <link href="{{url('templates/assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
   <link href="{{url('templates/assets/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
   <link href="{{url('templates/assets/select2/css/select2.min.css')}}" rel="stylesheet">
   <link href="{{url('templates/assets/selectize/css/selectize.css')}}" rel="stylesheet">
   <link href="{{url('templates/assets/selectize/css/selectize.bootstrap3.css')}}" rel="stylesheet">
+  <link href="{{url('templates/assets/plugins/datetimepicker-master/jquery.datetimepicker.css')}}" rel="stylesheet">
   <!-- Custom fonts for this template-->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
 </head>
 
 <body id="page-top">
