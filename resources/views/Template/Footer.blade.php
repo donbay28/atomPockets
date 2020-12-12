6423a0e416206d11f0d407931d17d17270979ed6
donbay28 <donbaysaputra28@gmail.com>
2020-12-12 19:25:00 +0700

    create tempalte

---
+++ resources/views/Template/Footer.blade.php
@@ -0,0 +1,31 @@
+<!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Maju Bersama Atomic Indonesia</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+   
+  
+  <script src="{{url('templates/assets/jquery-easing/jquery.easing.min.js')}}"></script>
+  <script src="{{url('templates/assets/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{url('templates/assets/js/sb-admin-2.min.js')}}"></script>
+  <script src="{{url('templates/assets/datatables/jquery.dataTables.min.js')}}"></script>
+  <script src="{{url('templates/assets/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+  <script src="{{url('templates/assets/js/demo/datatables-demo.js')}}"></script>
+  <script src="{{url('templates/assets/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+  <script src="{{url('templates/assets/selectize/js/selectize.min.js')}}"></script>
+  <script src="{{url('templates/assets/selectize/js/selectize.js')}}"></script>
+  <script src="{{url('templates/assets/select2/js/select2.min.js')}}"></script>
+  <script src="{{url('templates/assets/js/form-input.js')}}"></script>
+</body>
+
+</html>
---
+++ resources/views/Template/Header.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Atom Pockets</title>
+  <script src="{{url('templates/assets/jquery/jquery.min.js')}}"></script>
+  <link href="{{url('templates/assets/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+  <link href="{{url('templates/assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+  <link href="{{url('templates/assets/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+  <link href="{{url('templates/assets/select2/css/select2.min.css')}}" rel="stylesheet">
+  <link href="{{url('templates/assets/selectize/css/selectize.css')}}" rel="stylesheet">
+  <link href="{{url('templates/assets/selectize/css/selectize.bootstrap3.css')}}" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
